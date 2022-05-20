191f4fbac8d14c378b2e1ca8d52a9a009b64a4c7
Vinicius Barros <barros.vinicius0103@gmail.com>
2022-05-20 11:55:39 -0300

    feature: Creates the color pattern

---
+++ src/Ui/Theme/Colors.ts
@@ -0,0 +1,41 @@
+const Colors = {
+  Primary: {
+    Dark: '#5F0FFA' as const,
+    Normal: '#6E0DFB' as const,
+    Light: '#AA00FF' as const,
+  },
+  Secondary: {
+    Dark: '#1A0C30' as const,
+    Normal: '#2E0D62' as const,
+    Light: '#690DF8' as const,
+  },
+  Accent: '#FF2B00' as const,
+  Base: {
+    Dark: '#000000' as const,
+    GrayDarkest: '#131313' as const,
+    GrayDark: '#333333' as const,
+    Gray: '#585858' as const,
+    GrayLight: '#999999' as const,
+    GrayLighter: '#CCCCCC' as const,
+    GrayLightest: '#EFEFEF' as const,
+    GrayWhite: '#F2F2F2' as const,
+    Light: '#FFFFFF' as const,
+  },
+  Error: {
+    Dark: '#AF0000' as const,
+    Normal: '#DB0000' as const,
+    Light: '#FFC8C8' as const,
+  },
+  Success: {
+    Dark: '#004A19' as const,
+    Normal: '#1F8454' as const,
+    Light: '#4BFCA8' as const,
+  },
+  Alert: {
+    Dark: '#3E00C3' as const,
+    Normal: '#4E00F4' as const,
+    Light: '#DED1FA' as const,
+  },
+};
+
+export default Colors;
